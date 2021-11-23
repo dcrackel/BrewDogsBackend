ac83c723fdf274214652b3f5df0072815c51ae1c
Dwain Crackel <dcrackel@gmail.com>
2021-11-23 11:44:46 -0500

    Checking in for heroku server

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/backend")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+
+}
